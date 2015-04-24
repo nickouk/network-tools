c56773d063b51792cccdd36ac3534533e50d7a75
Nick O'Neill <nick@gone.me.uk>
2015-04-24 15:18:04 +0100

    Added support for device variables including telnet and local auth.

--- grab_configs.py
+++ grab_configs.py
@@ -3,7 +3,7 @@
 '''
 This module references a customer info file which holds a customer directory
 name and a list of management IPs. Prompt the user for SSH credentials then
-use those credntials to connect to each device in turn. Once connected retrieve
+use those credentials to connect to each device in turn. Once connected retrieve
 the config, lookup the hostname then store the config in the customer folder.
 
 Check timestamps and file contents to ensure the job has run correctly.
@@ -15,8 +15,10 @@ sample cust.info file:
 acme-ltd            # customer dir should already exist, must be the first line
 172.16.255.1
 172.16.255.2
-172.16.255.3:hp     # HP is for procurve kit, do not add :hp for Cisco/Juniper
+172.16.255.3:model,hp     # HP is for procurve kit, do not add :hp for Cisco/Juniper
 #172.16.255.10      #  This line is skipped becuase it is # out.
+172.16.255.4:conn,telnet:user,test,temp123
+172.16.255.5:user,test,temp123,enablepass
 
 
 '''
@@ -28,7 +30,7 @@ import datetime
 import time
 import sys
 import getpass
-
+import telnetlib
 
 
 # Functions
@@ -125,15 +127,20 @@ def raw_input_def(prompt, default):
     else:
         return usr_input
 
-def shell_send(cmd, wait, buf, ssh_shell):
+def shell_send(cmd, wait, buf, ssh_shell, shell_is_telnet):
     '''
-    Sends a command to the SSH shell and waits. Adds a newline
+    Sends a command to the SSH or telnet shell and waits. Adds a newline
     (simulates pressing enter)
     '''
 
-    ssh_shell.send(cmd + "\n")
-    time.sleep(wait)
-    return shell.recv(buf)
+    if not shell_is_telnet:
+        ssh_shell.send(cmd + "\n")
+        time.sleep(wait)
+        return ssh_shell.recv(buf)
+    else:
+        ssh_shell.write(cmd + "\r\n")
+        time.sleep(wait)
+        return ssh_shell.read_very_eager()
 
 def print_flush(flush_output):
     '''
@@ -172,12 +179,12 @@ if __name__ == "__main__":
 
     while True:
         cust = raw_input_def("Input the customer info file [%s]: " % def_cust, def_cust)
-        username = raw_input_def("Input SSH username [%s]: " % def_user, def_user)
-        password = getpass.getpass("Input SSH password: ")
+        input_username = raw_input_def("Input SSH username [%s]: " % def_user, def_user)
+        input_password = getpass.getpass("Input SSH password: ")
 
         print "\n"
         print cust
-        print username
+        print input_username
         print "**PASSWORD HIDDEN**"
 
         yesno = raw_input("\nAre these details correct [y/n]: ").lower()
@@ -214,17 +221,32 @@ if __name__ == "__main__":
 
         '''
         If the IP address contains a : then options have been added
-        currently only :hp is supported
         :hp means we use an interactive shell to obtain the config
+        otherwise we are using local password in the format user:pass:enable where enable is optional
         '''
 
-        hp = ""
+        hp = False
+        dev_specific = False
+        telnet = False
         if ":" in ip_addr:
-            ip_addr_list = ip_addr.split(":")
-            ip_addr = ip_addr_list[0]
-            if ip_addr_list[1] == "hp":
-                hp = True
-
+            type_list = ip_addr.split(":")
+            ip_addr = type_list[0]
+            for x in range(1, len(type_list)):
+                var = type_list[x]
+                var_list = var.split(",")
+                var_type = var_list[0]
+                if var_type == "model": hp = True
+                if var_type == "conn": telnet = True
+                if var_type == "user":
+                    dev_specific = True
+                    dev_username = var_list[1]
+                    dev_password = var_list[2]
+                    dev_enable = False
+                try:
+                    dev_enable = var_list[3]
+                except IndexError:
+                    pass
+                    #ok no enable
 
         '''
         Open the SSH Connection with some error handling
@@ -232,16 +254,48 @@ if __name__ == "__main__":
 
         print_flush("Establishing connection to %-15s > " % (ip_addr))
 
-        try:
-            ssh.connect(ip_addr, username=username, password=password, timeout=8)
-        except paramiko.ssh_exception.AuthenticationException:
-            print "Authentication failed."
-            status_update(cust_dir, ip_addr, "", "*** Authentication failed. ***")
-            continue
-        except socket.error:
-            print "Could not connect."
-            status_update(cust_dir, ip_addr, "", "*** Connection error. ***")
-            continue
+        username = input_username
+        password = input_password
+        enable = ""
+
+        if dev_specific:
+            username = dev_username
+            password = dev_password
+            enable = dev_enable
+
+        if not telnet:
+            try:
+                ssh.connect(ip_addr, username=username, password=password, timeout=8)
+            except paramiko.ssh_exception.AuthenticationException:
+                print "Authentication failed (SSH)."
+                status_update(cust_dir, ip_addr, "", "*** Authentication failed. ***")
+                continue
+            except socket.error:
+                print "Could not connect (SSH)."
+                status_update(cust_dir, ip_addr, "", "*** Connection error (SSH). ***")
+                continue
+        else:
+            try:
+                shell = telnetlib.Telnet(ip_addr, 23, 4)
+            except socket.error:
+                print "Could not connect (Telnet)."
+                status_update(cust_dir, ip_addr, "", "*** Connection error (Telnet). ***")
+                continue
+
+            shell.read_very_eager()
+            output = shell_send(username, 1, 500, shell, telnet)
+            auth_failed = re.search(r"Login invalid", output)
+            if auth_failed:
+                print "Authentication failed (Telnet)."
+                status_update(cust_dir, ip_addr, "", "*** Authentication failed. ***")
+                continue
+
+            output = shell_send(password, 1, 500, shell, telnet)
+            auth_failed = re.search(r"Login invalid", output)
+            if auth_failed:
+                print "Authentication failed (Telnet)."
+                status_update(cust_dir, ip_addr, "", "*** Authentication failed. ***")
+                continue
 
         print_flush("[ Connection established ]")
 
@@ -257,23 +311,38 @@ if __name__ == "__main__":
         for HP devices for cisco we will use the 'cleaner' exec_command method
         '''
 
-        if not hp:
+        if not hp and not enable and not telnet:
             # Cisco Device
             stdin, stdout, stderr = ssh.exec_command("sh run")
             config = stdout.read()
         else:
-            # HP Device
-            shell = ssh.invoke_shell(width=200, height=99999)
-            # Strip MOTD
-            output = shell.recv(2000)
+            # HP Device, or enable password, or telnet
+            
+            if not telnet:
+                shell = ssh.invoke_shell(width=200, height=99999)
+
+                # Strip MOTD
+                output = shell.recv(2000)
+
+            # Enable mode
+            if enable:
+                shell_send("enable 15", 1 , 1000, shell, telnet)
+                shell_send(enable, 1, 1000, shell, telnet)
 
             # Press enter,turn off paging,grab config
-            shell_send("", 1, 1000, shell)
-            shell_send("no page", 1, 500, shell)
-            config = shell_send("show run", 6, 65535, shell)
+            shell_send("", 1, 1000, shell, telnet)
+
+            if hp:
+                paging = "no page"
+            else:
+                paging = "term len 0"
+
+            shell_send(paging, 1, 500, shell, telnet)
+            config = shell_send("show run", 6, 65535, shell, telnet)
             shell.close()
 
-            config = clean_ansi(config)
+            if hp:
+                config = clean_ansi(config)
 
         # Use regex to find the hostname in the config
         try:
